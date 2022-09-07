40cbcd84aa565782e9ccc5a4d36d53f23e1c31f1
nasifahmedoney <nasif.ahmed@live.com>
2022-09-07 22:39:18 +0600

    read from database-incomplete

--- react-firebase-crud/src/components/Form.js
+++ react-firebase-crud/src/components/Form.js
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { database } from '../firebase'
-import { ref, set } from 'firebase/database';
+import { ref, set, onValue } from 'firebase/database';
 import { uid } from 'uid';
 
 
 function Form(){
     const [inputVal,setInputVal] = useState('');
+    const [todolist,setTodolist] = useState([]);
 
     const setValueOnChange = (e)=>{
         setInputVal(e.target.value);
     };
 
     const handleOnSubmit=()=>{
+        //write data
         const uuid = uid();
             set(ref(database,`/${uuid}`),{
                 title: inputVal,
@@ -19,10 +21,32 @@ function Form(){
             })
             setInputVal('')            
     };
+    /*
+    check useEffect, read data from firebase 
+     */
+    useEffect(()=>{
+        onValue(ref(database),(snapshot)=>{
+            const data = snapshot.val();
+            setTodolist([data]);
+            // console.log(todolist);
+            console.log(data)
+        })
+    },[])
+    
     return(
         <div>
             <input type="text" onChange={setValueOnChange} value={inputVal}/>
             <button onClick={handleOnSubmit}>submit</button>
+            <div>
+                <h2>Data from the database</h2>
+                <ul>
+                    {/* {todolist.map((list)=>{
+                        return(<li>
+                            {list.title}
+                        </li>);
+                    })} */}
+                </ul>
+            </div>
         </div>
     );
 }
